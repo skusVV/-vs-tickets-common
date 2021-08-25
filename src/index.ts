d970bd6f6548b1bd1a0661b6670277d0553a5188
skusVV <skus1992@gmail.com>
2021-08-25 12:41:10 +0300

    updated index

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export * from './errors/not-found.error';
 export * from './errors/database-connection.error';
 export * from './errors/not-authorized.error';
 export * from './errors/not-found.error';
-
 export * from './middlewares/currennt-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
