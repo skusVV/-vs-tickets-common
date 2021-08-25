bb0235b7136c4802f3d7d7786963e73edc66605f
skusVV <skus1992@gmail.com>
2021-08-25 12:04:17 +0300

    test publish

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+interface Color {
+    red: number;
+    blue: number;
+    green: number;
+}
+
+const color: Color = {
+    red: 2,
+    green: 133,
+    blue: 4
+}
+console.log(color);
+
+export default color;
