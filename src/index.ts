f08be8a4c4672a6cf0cd2ed61e319a151406047e
skusVV <skus1992@gmail.com>
2021-08-29 13:33:55 +0300

    added nats events

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ export * from './errors/not-found.error';
 export * from './errors/database-connection.error';
 export * from './errors/not-authorized.error';
 export * from './errors/not-found.error';
+
 export * from './middlewares/currennt-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/subjects';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
